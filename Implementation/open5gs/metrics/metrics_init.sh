e373bb993cd456ac5bef71d134bcce99547dc9c2
David <davidtruhlarac@gmail.com>
2025-05-08 12:39:40 +0200

    fix:Windows_issues

--- Implementation/open5gs/metrics/metrics_init.sh
+++ Implementation/open5gs/metrics/metrics_init.sh
@@ -12,6 +12,7 @@ sed -i 's|SMF_IP|'$SMF_IP'|g' /config/prometheus.yml
 sed -i 's|MME_IP|'$MME_IP'|g' /config/prometheus.yml
 sed -i 's|PCF_IP|'$PCF_IP'|g' /config/prometheus.yml
 sed -i 's|UPF_IP|'$UPF_IP'|g' /config/prometheus.yml
+sed -i 's|NETWORK_WATCHER_IP|'$NETWORK_WATCHER_IP'|g' /config/prometheus.yml
 
 ./prometheus --config.file=/config/prometheus.yml --web.enable-remote-write-receiver --storage.tsdb.allow-overlapping-blocks
 
